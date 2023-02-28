7a3ec07670e5c8ec70043cc412bd6abb819442ca
Rasmus Thomsen <oss@cogitri.dev>
2023-02-28 12:31:32 +0100

    export DEVICE_LOCAL_ADDRESS_IPV6

--- fritz-dyndns/run.sh
+++ fritz-dyndns/run.sh
@@ -7,5 +7,6 @@ export CLOUDFLARE_API_EMAIL=$(bashio::config cloudflare_api_email)
 export CLOUDFLARE_API_TOKEN=$(bashio::config cloudflare_api_key)
 export CLOUDFLARE_ZONES_IPV4=$(bashio::config 'cloudflare_zones_ipv4|join(", ")')
 export CLOUDFLARE_ZONES_IPV6=$(bashio::config 'cloudflare_zones_ipv6|join(", ")')
+export DEVICE_LOCAL_ADDRESS_IPV6=$(bashio::config 'device_local_address_ipv6')
 
 /app/server
